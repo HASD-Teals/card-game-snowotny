872d48b47ee5d82da458f679c1fb401ceff71baa
Sarah Nowotny <90402909+snowotny@users.noreply.github.com>
2022-02-03 09:34:48 -0600

    Add files via upload
    up to part 12

--- ClientCard.java
+++ ClientCard.java
@@ -1,16 +1,6 @@
 public class ClientCard {
     public static void main(String[] args) throws Exception{
-        card King_of_Hearts = new card();
-        King_of_Hearts.setColor("red");
-        King_of_Hearts.setRank("K");
-        King_of_Hearts.setSymbol("heart");
-        King_of_Hearts.setValue(10);
-        King_of_Hearts.setPlayable(true);
-        System.out.println("color: " + King_of_Hearts.getColor());
-        System.out.println("rank: " + King_of_Hearts.getRank());
-        System.out.println("symbol: " + King_of_Hearts.getSymbol());
-        System.out.println("value: " + King_of_Hearts.getValue());
-        System.out.println("playable: " + King_of_Hearts.getPlayable());
-        
+        card Card1 = new card("red","King","Heart",10,true);
+        System.out.println( Card1.toString());
     }
 }
