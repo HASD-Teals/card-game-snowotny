da58a4035c26abee1a6c7ce627ae0cf38af6aac9
Sarah Nowotny <90402909+snowotny@users.noreply.github.com>
2022-02-07 09:43:36 -0600

    Add files via upload

---
+++ CardGameClient.java
@@ -0,0 +1,13 @@
+package src;
+public class CardGameClient {
+    public static void main(String[] args) throws Exception {
+       Deck deck1 = new Deck();
+       Deck deck2 = new Deck();
+       for(int i=0;i<deck1.length;i++){
+           //System.out.println(Deck deck1[i]);
+       }
+       for(int i=0;i<deck2.length;i++){
+           //System.out.println(Deck deck2[i]);
+       }
+    }
+}
